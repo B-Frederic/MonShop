05bd88f41953aa19c6a4e29f579e5bb4f97890b4
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-06-07 11:58:53 +0200

    patch: design error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
 
     return (
         <div className="flex items-center justify-center text-center h-[65vh]">
-            <div className="text-[2rem]">
+            <div className="p-[40px] bg-gray-800 rounded-xl font-bold text-[2rem] text-red-600">
                 <p><span className="font-bold">Oups !!</span><br/>Cette page n'existe pas.</p>
             </div>
         </div>
