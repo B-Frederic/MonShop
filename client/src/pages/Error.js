65608737dd77d59ecbd1bc2b92363cf2d72a0db2
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-06-07 12:03:00 +0200

    fix: error responsive

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Error = () => {
 
     return (
-        <div className="flex items-center justify-center text-center h-[65vh]">
-            <div className="p-[40px] bg-gray-800 rounded-xl font-bold text-[2rem] text-red-600">
+        <div className="flex items-center justify-center text-center h-[65vh] px-[10px]">
+            <div className="p-[40px] bg-gray-800 rounded-[1.5rem] font-bold text-[2rem] text-red-600">
                 <p><span className="font-bold">Oups !!</span><br/>Cette page n'existe pas.</p>
             </div>
         </div>
